adce88d447799436e890cfa700191221bd102769
Zan <vans914@gmail.com>
2023-10-31 19:20:16 +0800

    新增歌詞效果調整css第一首完成

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function startGame(){
     setTimeout(function(){
         var q1song = new Audio("./audios/烏梅子醬.MP3");
         q1song.play();
-    },3000);
+    },2000);
 }
 
 $(document).ready(function(){
@@ -37,3 +37,25 @@ Swal.fire({
     showConfirmButton: false,
     timer: 4500
   })
+
+  
+document.addEventListener('DOMContentLoaded',function(){
+    var lrc1Elements = document.querySelectorAll('.lrc1section .lrc1 li');
+    lrc1Elements.forEach(function(element){
+        element.addEventListener('animationend',function(){
+            var q1sections = document.querySelectorAll('.q1section');
+            q1sections.forEach(function(q1section){
+                q1section.style.display = 'block';
+            })        
+        })
+    });   
+})
+
+function q1answer(){
+    var q1answer = new Audio("./audios/烏梅子醬2.MP3")
+    q1answer.play();
+    var lrc11section = document.getElementsByClassName('lrc11section')[0];
+    lrc11section.style.display ="block";
+    var lrc1section = document.getElementsByClassName('lrc1section')[0];
+    lrc1section.style.display = "none" ;
+}
